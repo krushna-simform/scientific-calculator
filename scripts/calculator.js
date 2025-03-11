b2b804aa45b504489b97e58a885d2f35e1461cb5
krushna rathod <krushna.rathod@simformsolutions.com>
2025-03-11 17:35:12 +0530

    feture: create calculator class and import it in index.js file

---
+++ scripts/calculator.js
@@ -0,0 +1,20 @@
+class Calculator {
+    constructor (calculatorSelector, inputFieldSelector, buttonSelectors) {
+        // DOM Selectors
+        this.calculatorContainer = document.querySelector(calculatorSelector);
+        this.inputField = document.querySelector(inputFieldSelector);
+        this.buttons = document.querySelectorAll(buttonSelectors);
+
+        this.currentInput = "";
+        
+        this.init();
+    }
+
+    init () {
+        this.buttons.forEach(button => {
+            button.addEventListener("click", (e) => this.handleButtonClick(e));
+        });
+    }
+}
+
+export { Calculator };

1d38d15fc1ca020251e401e113957d836a85f541
krushna rathod <krushna.rathod@simformsolutions.com>
2025-03-11 19:19:22 +0530

    feture: create history.js file

---
+++ scripts/history.js
@@ -0,0 +1,26 @@
+class History {
+    constructor (history, key) {
+        this.history = history;
+        this.key = key
+
+        this.init();
+    }
+
+    init () {
+        try {
+            const storedHistory = JSON.parse(localStorage.getItem(this.key)) || [];
+            if (Array.isArray(storedHistory)) {
+                this.history.push(...storedHistory);
+            }
+        } catch (err) {
+            this.history = [];
+        }
+    }
+
+    historySave () {
+        localStorage.setItem(this.key, JSON.stringify(this.history));
+    }
+
+}
+
+export { History };

39c263b1f1656003e054533436ee105812f30e84
krushna rathod <krushna.rathod@simformsolutions.com>
2025-03-20 10:21:51 +0530

    feature: add expression for PI

--- scripts/expression.js
+++ scripts/expression.js
@@ -4,6 +4,9 @@ class Expression {
             // Remove 0 value exp: 03 => 3
             expression = expression.replace(/\b0+(\d+)/g, "$1");
 
+            // Replace π with Math.PI
+            expression = expression.replace(/π/g, "Math.PI");
+
             if (eval(expression) == "Infinity") {
                 alert("Inifinity | Invalid expression");
             } else {
